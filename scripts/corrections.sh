573989b77e27aa1e6768fd74c5e680cb68b428d7
CG <cgordon.13@gmail.com>
2024-03-11 23:29:16 -0400

    Adding start of corrections script

---
+++ scripts/corrections.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+find . -type f -name "*.txt" -exec sed -i'' -e 's/gang world/Gangrel/g' {} +
+find . -type f -name "*.txt" -exec sed -i'' -e 's/gangler/Gangrel/g' {} +
+find . -type f -name "*.txt" -exec sed -i'' -e 's/gangrol/Gangrel/g' {} +
+find . -type f -name "*.txt" -exec sed -i'' -e 's/gangril/Gangrel/g' {} +
+find . -type f -name "*.txt" -exec sed -i'' -e 's/gangball/Gangrel/g' {} +
+find . -type f -name "*.txt" -exec sed -i'' -e 's/gangirl/Gangrel/g' {} +
+find . -type f -name "*.txt" -exec sed -i'' -e 's/gang role/Gangrel/g' {} +
+find . -type f -name "*.txt" -exec sed -i'' -e 's/gang roll/Gangrel/g' {} +
+find . -type f -name "*.txt" -exec sed -i'' -e 's/gang girl/Gangrel/g' {} +
